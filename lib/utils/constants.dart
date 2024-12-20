6ac83ad59badda41390139c14df6e1bc5dc2a673
Utsav Hingar <utsavhingar22@gmail.com>
2024-12-20 22:28:01 +0530

    Replaces the Key

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,7 +2,7 @@ const String baseUrl = 'https://google-translate1.p.rapidapi.com/language/transl
 
 const Map<String, String> apiHeaders = {
   'content-type': 'application/x-www-form-urlencoded',
-  'x-rapidapi-key': '82834240c6msh1d3d36fd8442170p16b3f1jsn87098074663b',
+  'x-rapidapi-key': 'YOUR_API_KEY',
   'x-rapidapi-host': 'google-translate1.p.rapidapi.com',
   'Accept-Encoding': 'application/gzip',
 };
